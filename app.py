980e08b676aff85714e75c103c46d05516cad89f
AbdoMagdy <abdoman46@hotmail.com>
2020-01-12 03:06:05 +0200

    set tiemout

--- app.py
+++ app.py
@@ -44,8 +44,8 @@ def handle_incoming_messages():
 					# HANDLE NORMAL MESSAGES HERE
 					if messaging_event['message'].get('text'):
 						bot.send_action(sender_id, 'mark_seen')
-						bot.send_action(sender_id, 'typing_on', time_out=1)
-						bot.send_action(sender_id, 'typing_off')
+						bot.send_action(sender_id, 'typing_on')
+						bot.send_action(sender_id, 'typing_off'time_out=1.50)
 						bot.send_generic_message(sender_id, test.elements)
 						
 	return "ok", 200
