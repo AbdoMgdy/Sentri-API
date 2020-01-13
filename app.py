4605c661057e41e5031b013e11b576240a8f3f4e
AbdoMagdy <abdoman46@hotmail.com>
2020-01-13 08:27:39 +0200

    testing JSON formats

--- app.py
+++ app.py
@@ -39,9 +39,10 @@ def handle_incoming_messages():
 				#recipient_id = messaging_event['recipient']['id']
 
 				if messaging_event.get('message'):
+					# HANDLE Quick Replies HERE
 					if messaging_event['message'].get('quick_reply'):
 						bot.send_before_message(sender_id)
-						block_name = messaging_event['postback'].get('payload')
+						block_name = messaging_event['quick_reply'].get('payload')
 						print('quick reply')
 						print(block_name)
 						block_obj = eval(eval(block_name))
@@ -49,12 +50,13 @@ def handle_incoming_messages():
 						block_obj.send(sender_id)
 						return "ok", 200
 
-					# HANDLE NORMAL MESSAGES HERE
+					# HANDLE Text MESSAGES HERE
 					elif messaging_event['message'].get('text'):
 						bot.send_before_message(sender_id)
 						menu.send(sender_id)
 						return "ok", 200
 				elif messaging_event.get('postback'):
+					# HANDLE POSTBACK HERE
 					bot.send_before_message(sender_id)
 					block_name = messaging_event['postback'].get('payload')
 					print('it came here')
