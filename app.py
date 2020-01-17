ea4b99533ed453f411ca39ac3beb6d98edd8fed6
AbdoMagdy <abdoman46@hotmail.com>
2020-01-17 08:38:15 +0200

    lookin for bug :$

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from models.bot import Bot
 from resources.menu import *
 
 
-app = Flask(__name__)
-# SECRET_KEY = os.urandom(32)
-# app.config['SECRET_KEY'] = SECRET_KEY
+app = Flask(__name__, template_folder='templates')
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_UR', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -26,8 +26,8 @@ blocks = {
 }
 
 
-# sender_id = ''
-# order_number = ''
+sender_id = ''
+order_number = ''
 
 
 @app.route('/', methods=['GET'])
@@ -53,18 +53,18 @@ def handle_incoming_messages():
 
             for messaging_event in messaging:
 
-                # global sender_id
+                global sender_id
                 sender_id = messaging_event['sender']['id']
 
-                # user = User.find_by_psid(sender_id)
-                # order = Order(sender_id)
-                # if not user:
-                #     user = User(sender_id)
-                #     user.get_info()
-                #     user.add()
-                # if not user.orders:
-                #     global order_number
-                #     order_number = order['number']
+                user = User.find_by_psid(sender_id)
+                order = Order(sender_id)
+                if not user:
+                    user = User(sender_id)
+                    user.get_info()
+                    user.add()
+                if not user.orders:
+                    global order_number
+                    order_number = order['number']
 
                 if messaging_event.get('message'):
                     # HANDLE QUICK REPLIES HERE
@@ -93,30 +93,30 @@ def handle_incoming_messages():
     return "ok", 200
 
 
-# @app.route('/webview/order/<string:item>', methods=['GET', 'POST'])
-# def show_webview(item):
-#     form = OrderForm()
-#     if form.validate_on_submit():
-#         return redirect('/add_to_order/{}'.format(item))
-#     return render_template('order.jinja', item=item, form=form)
+@app.route('/webview/order/<string:item>', methods=['GET', 'POST'])
+def show_webview(item):
+    form = OrderForm()
+    if form.validate_on_submit():
+        return redirect('/add_to_order/{}'.format(item))
+    return render_template('order.jinja', item=item, form=form)
 
 
-# @app.route('/add_to_order/<string:item>', methods=['POST'])
-# def add_to_order(item):
-#     qty = request.form.get['quantity']
-#     spicy = request.form.get['spicy']
-#     notes = request.form.get['notes']
-#     order = Order.find_by_number(order_number)
-#     if not order.is_confirmed:
-#         order.add_item(item, qty, spicy, notes)
-#     return 'ok', 200
+@app.route('/add_to_order/<string:item>', methods=['POST'])
+def add_to_order(item):
+    qty = request.form.get['quantity']
+    spicy = request.form.get['spicy']
+    notes = request.form.get['notes']
+    order = Order.find_by_number(order_number)
+    if not order.is_confirmed:
+        order.add_item(item, qty, spicy, notes)
+    return 'ok', 200
 
 
-# @app.route('/confirm_order', methods=['POST'])
-# def confirm_order():
-#     order = Order.find_by_number(order_number)
-#     if not order.is_confirmed:
-#         order.confirm
+@app.route('/confirm_order', methods=['POST'])
+def confirm_order():
+    order = Order.find_by_number(order_number)
+    if not order.is_confirmed:
+        order.confirm
 
 
 if __name__ == "__main__":
