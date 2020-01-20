dc6c4bc35e3c5a693d7def51159937e288afcb89
AbdoMagdy <abdoman46@hotmail.com>
2020-01-20 20:08:44 +0200

    different forms

--- app.py
+++ app.py
@@ -128,8 +128,10 @@ def add_to_order(food, item, price):
         notes = request.form.get('notes')
     if request.form.get('combo') is None:
         combo = 0
-    # elif request.form.get('combo') is not None:
-    #     combo = request.form.get('combo')
+    elif request.form.get('combo') is not None:
+        combo = request.form.get('combo')
+    if request.form.get('size') is not None:
+        spicy = request.form.get('size')
 
     order = Order.find_by_number(order_number)
     print(food)
