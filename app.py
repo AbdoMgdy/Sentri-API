9eca6a79a91d6712f32ae8cf501914fa832ce8d0
AbdoMagdy <abdoman46@hotmail.com>
2020-01-23 18:36:37 +0200

    edit order

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ class CustomFlask(Flask):
     ))
 
 
-app = CustomFlask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates')
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
