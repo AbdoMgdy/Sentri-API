434c72057517b40194408a60bcde71df1682c969
AbdoMagdy <abdoman46@hotmail.com>
2020-01-24 18:34:46 +0200

    edit order

--- resources/buttons.py
+++ resources/buttons.py
@@ -4,16 +4,16 @@ family_btn = ButtonTemplate()
 family_btn.add_postback(**{'Show Menu عرض المنيو': 'family_menu'})
 
 burger_btn = ButtonTemplate()
-burger_btn.add_postback(**{'Back': 'family_menu'})
+
 burger_btn.add_web_url(
     **{'Order Now!': 'https://trex-chat-bot.herokuapp.com/webview/order/sandwich/burger/100.0'})
 
 meal_btn = ButtonTemplate()
-meal_btn.add_postback(**{'Back': 'family_menu'})
+
 meal_btn.add_web_url(
     **{'Order Now!': 'https://trex-chat-bot.herokuapp.com/webview/order/meal/9-chicekn/100.0'})
 sauce_btn = ButtonTemplate()
-sauce_btn.add_postback(**{'Back': 'family_menu'})
+
 sauce_btn.add_web_url(
     **{'Order Now!': 'https://trex-chat-bot.herokuapp.com/webview/order/sauce/hot-sauce/100.0'})
 
