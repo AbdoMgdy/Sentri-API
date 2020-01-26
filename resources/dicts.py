994d48d8ff3a1059d2b98ab7a4c51e4a0e55c1c7
AbdoMagdy <abdoman46@hotmail.com>
2020-01-26 17:55:55 +0200

    better structure

--- resources/dicts.py
+++ resources/dicts.py
@@ -1,4 +1,5 @@
-from menu import main_menu, family_menu, confirm_block
+from resources.menu import main_menu, family_menu
+from resources.buttons import confirm_block
 blocks = {
     'get_started': main_menu,
     'main_menu': main_menu,
