0d4609a35e18c3fdcd58e3294b7875d7e3c7f793
AbdoMagdy <abdoman46@hotmail.com>
2020-01-26 18:27:17 +0200

    better structure

--- app.py
+++ app.py
@@ -27,15 +27,15 @@ VERIFICATION_TOKEN = "test"
 bot = Bot()
 
 
-blocks = {
-    'get_started': main_menu,
-    'main_menu': main_menu,
-    'family_menu': family_menu,
-    'confirm_block': confirm_block,
-}
+# blocks = {
+#     'get_started': main_menu,
+#     'main_menu': main_menu,
+#     'family_menu': family_menu,
+#     'confirm_block': confirm_block,
+# }
 
 
-orders = {}
+# orders = {}
 
 restaurant = ''
 
