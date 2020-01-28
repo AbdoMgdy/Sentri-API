8bce70fc8ea5af5d05fd617d508c65d62caf1441
AbdoMagdy <abdoman46@hotmail.com>
2020-01-28 19:59:07 +0200

    fix

--- models/media.py
+++ models/media.py
@@ -20,10 +20,10 @@ class MediaTemplate(Bot):
     def __init__(self, url=''):
         super().__init__()
         self.template = copy(template['value'])
-        self.template['attachment']['payload']['elements'] = {
+        self.template['attachment']['payload']['elements'] = [{
             'media_type': 'image',
             'url': url
-        }
+        }]
 
     def send(self, recipient_id):
         super().send_message(recipient_id, self.template)
