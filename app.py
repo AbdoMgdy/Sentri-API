12eb71fb4aabd691c9a0879da56f509ba548c9ff
AbdoMagdy <abdoman46@hotmail.com>
2020-01-30 16:34:03 +0200

    Loging User

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ bot = Bot()
 restaurant = ''
 
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('show_orders'))
