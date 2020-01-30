9a577dd1bd98b29ca80a85ca234a0da231716336
AbdoMagdy <abdoman46@hotmail.com>
2020-01-30 17:40:04 +0200

    Login Form

--- models/data_models.py
+++ models/data_models.py
@@ -19,8 +19,7 @@ class LoginUser(UserMixin, db.Model):
 
     def __init__(self, user_name, password):
         self.username = user_name
-        self.password = ''
-        self.password_hash = generate_password_hash(password)
+        self.password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
