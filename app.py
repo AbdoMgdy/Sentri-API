4c9e4316c6d4f2663f48ace49fb62f3c840ec052
AbdoMagdy <abdoman46@hotmail.com>
2020-02-04 17:07:05 +0200

    Socket Chat Test

--- app.py
+++ app.py
@@ -38,6 +38,21 @@ bot = Bot()
 restaurant = ''
 
 
+@app.rout('/socket')
+def socket():
+    return render_template('socket.jinja')
+
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
