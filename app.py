a74b7744077134e5bf9b454282a6a05e6361a9c6
AbdoMagdy <abdoman46@hotmail.com>
2020-02-17 22:52:04 +0200

    V2.0

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def handle_incoming_messages():
             print('Found it')
             block = blocks[block_name]
             bot.send_template_message(sender_id, block)
-            # print(bot.send_template_message(sender_id, block))
+            print(bot.send_template_message(sender_id, block))
 
         return "postback", 200
     else:
