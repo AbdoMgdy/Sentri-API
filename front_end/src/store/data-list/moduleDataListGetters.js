7e6f5cb2623e9cf74c2564dd3c5d9762b9645c0c
AbdoMagdy <abdoman46@hotmail.com>
2020-02-20 22:39:08 +0200

    ok

--- front_end/src/store/data-list/moduleDataListGetters.js
+++
@@ -1,9 +0,0 @@
-/*=========================================================================================
-  File Name: moduleCalendarGetters.js
-  Description: Calendar Module Getters
-==========================================================================================*/
-
-
-export default {
-  // getItem: state => (productId) => state.products.find((product) => product.id == productId),
-}
