77c892dadcfc90718d987bbdcbdd569891e4798b
AbdoMagdy <abdoman46@hotmail.com>
2020-03-06 03:27:54 +0200

    page_access_token

--- app/customer/controller.py
+++ app/customer/controller.py
@@ -19,7 +19,7 @@ class CustomerResource(Resource):
         # look for customer
         customer = Customer.find_by_psid(psid)
         vendor = customer.vendor
-        bot = Bot(access_token=vendor.access_token)
+        bot = Bot(access_token=vendor.page_access_token)
 
         # creat order object and fill it from temp dict
         order = Order(psid, vendor.page_id)
