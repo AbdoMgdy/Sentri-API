ef61a195f5ae1430ddddaf29c99e5178fa722e19
AbdoMagdy <abdoman46@hotmail.com>
2020-03-06 07:32:26 +0200

    wow this is Fun !

--- app/customer/schema.py
+++ app/customer/schema.py
@@ -1,7 +1,9 @@
 from app import ma
 from .model import Customer
+# from app.order.schema import OrderSchema
 
 
 class CustomerSchema(ma.ModelSchema):
     class Meta:
         model = Customer
+    # orders = ma.Nested(OrderSchema, many=True)
