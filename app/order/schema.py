e847a9a35ced583a26144cc577b9c5dd864632d7
AbdoMagdy <abdoman46@hotmail.com>
2020-03-06 07:29:11 +0200

    ok

--- app/order/schema.py
+++ app/order/schema.py
@@ -1,11 +1,11 @@
 from app import ma
 from .model import Order
 # from app.vendor.schema import VendorSchema
-# from app.customer.schema import CustomerSchema
+from app.customer.schema import CustomerSchema
 
 
 class OrderSchema(ma.ModelSchema):
     class Meta:
         model = Order
-    # customer = ma.Nested(CustomerSchema)
+    customer = ma.Nested(CustomerSchema)
     # vendor = ma.Nested(VendorSchema)
