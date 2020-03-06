80a44d8b9591c6bdce01f1509d021883e9d264cd
AbdoMagdy <abdoman46@hotmail.com>
2020-03-06 11:54:44 +0200

    ok

--- app/resources/helper_functions.py
+++ app/resources/helper_functions.py
@@ -3,7 +3,7 @@ from app.order import Order, OrderSchema
 from app.customer import Customer
 from app.vendor import Vendor
 from app.models.bot import Bot
-from firebase_admin import messaging, firebase_admin
+import firebase_admin
 import ast
 
 
@@ -152,6 +152,6 @@ def send_order_to_vendor(result, fcm_token):
         app = firebase_admin.get_app()
     except ValueError as e:
         firebase_admin.initialize_app()
-    msg = messaging.Message(data=order, token=fcm_token)
-    msg_id = messaging.send(msg)
+    msg = firebase_admin.messaging.Message(data=order, token=fcm_token)
+    msg_id = firebase_admin.messaging.send(msg)
     return msg_id
