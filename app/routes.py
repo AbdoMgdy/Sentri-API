52b13c54c53b21c2e95164e6223971bdc074b3fe
AbdoMagdy <abdoman46@hotmail.com>
2020-06-12 06:32:50 +0200

    add catalog routes register

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 def register_routes(app, api):
+    from app.catalog import register_routes as attach_catalog
     from app.order import register_routes as attach_order
     from app.customer import register_routes as attach_customer
     from app.vendor import register_routes as attach_vendor
@@ -6,6 +7,7 @@ def register_routes(app, api):
     from app.webview import routes as webview_routes
 
     # Add routes
+    attach_catalog(app, api)
     attach_order(app, api)
     attach_customer(app, api)
     attach_vendor(app, api)
