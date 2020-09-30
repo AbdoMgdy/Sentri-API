31e97f00fe7ccb8c126e662f744506c1e78fee8c
AbdoMgdy <abdoman46@hotmail.com>
2020-09-30 21:22:36 +0200

    refactor catalog

--- app/catalog/controller.py
+++ app/catalog/controller.py
@@ -13,120 +13,120 @@ api = Namespace('Catalog')
 
 @api.route('/<string:resource>')
 class CatalogResource(Resource):
-    @jwt_required
-    def get(self, resource):
-        identity = get_jwt_identity()
-        print(identity)
-        vendor = Vendor.find_by_uid(identity)
-        print(vendor)
-        catalog = Catalog.find_by_page_id(vendor.page_id)
-        print(catalog)
-        if not catalog:
-            return 'Catalog Not Found'
-        if resource == 'items':
-            return jsonify(catalog.items)
-        elif resource == 'categories':
-            return jsonify(catalog.categories)
-        elif resource == 'knowledge':
-            return jsonify(catalog.knowledge)
+    # @jwt_required
+    # def get(self, resource):
+    #     identity = get_jwt_identity()
+    #     print(identity)
+    #     vendor = Vendor.find_by_uid(identity)
+    #     print(vendor)
+    #     catalog = Catalog.find_by_page_id(vendor.page_id)
+    #     print(catalog)
+    #     if not catalog:
+    #         return 'Catalog Not Found'
+    #     if resource == 'items':
+    #         return jsonify(catalog.items)
+    #     elif resource == 'categories':
+    #         return jsonify(catalog.categories)
+    #     elif resource == 'knowledge':
+#     #         return jsonify(catalog.knowledge)
 
-    @jwt_required
-    def post(self, resource):
-        data = request.get_json()
-        print(data)
-        identity = get_jwt_identity()
-        print(identity)
-        vendor = Vendor.find_by_uid(identity)
-        print(vendor)
-        catalog = Catalog.find_by_page_id(vendor.page_id)
-        print(catalog)
-        if data['img'] is None:
-            data['img'] = ""
-        if not catalog:
-            return 'Catalog Not Found'
-            print('Catalog Not Found')
-        if resource == 'items':
-            catalog.add_item(data['category'], data['title'],
-                             data['subtitle'], data['price'], data['img'], data['in_stock'])
-            print('Item Added Successfully')
-            return f'{resource} added successfully'
-        elif resource == 'categories':
-            catalog.add_category(data['title'],
-                                 data['subtitle'], data['img'])
-            print('Category Added Successfully')
-        return f'{resource} added successfully'
+#     @jwt_required
+#     def post(self, resource):
+#         data = request.get_json()
+#         print(data)
+#         identity = get_jwt_identity()
+#         print(identity)
+#         vendor = Vendor.find_by_uid(identity)
+#         print(vendor)
+#         catalog = Catalog.find_by_page_id(vendor.page_id)
+#         print(catalog)
+#         if data['img'] is None:
+#             data['img'] = ""
+#         if not catalog:
+#             return 'Catalog Not Found'
+#             print('Catalog Not Found')
+#         if resource == 'items':
+#             catalog.add_item(data['category'], data['title'],
+#                              data['subtitle'], data['price'], data['img'], data['in_stock'])
+#             print('Item Added Successfully')
+#             return f'{resource} added successfully'
+#         elif resource == 'categories':
+#             catalog.add_category(data['title'],
+#                                  data['subtitle'], data['img'])
+#             print('Category Added Successfully')
+#         return f'{resource} added successfully'
 
-    @jwt_required
-    def put(self, resource):
-        data = request.get_json()
-        print(data)
-        identity = get_jwt_identity()
-        print(identity)
-        vendor = Vendor.find_by_uid(identity)
-        print(vendor)
-        catalog = Catalog.find_by_page_id(vendor.page_id)
-        print(catalog)
-        if not catalog:
-            return 'Catalog Not Found'
-            print('Catalog Not Found')
-        if resource == 'items':
-            catalog.edit_item(data)
-            print('Item Edited Successfully')
-            return f'{resource} Edited successfully'
-        elif resource == 'categories':
-            catalog.edit_category(data)
-            print('Category Edited Successfully')
+#     @jwt_required
+#     def put(self, resource):
+#         data = request.get_json()
+#         print(data)
+#         identity = get_jwt_identity()
+#         print(identity)
+#         vendor = Vendor.find_by_uid(identity)
+#         print(vendor)
+#         catalog = Catalog.find_by_page_id(vendor.page_id)
+#         print(catalog)
+#         if not catalog:
+#             return 'Catalog Not Found'
+#             print('Catalog Not Found')
+#         if resource == 'items':
+#             catalog.edit_item(data)
+#             print('Item Edited Successfully')
+#             return f'{resource} Edited successfully'
+#         elif resource == 'categories':
+#             catalog.edit_category(data)
+#             print('Category Edited Successfully')
 
-        return f'{resource} Edited successfully'
+#         return f'{resource} Edited successfully'
 
-    @jwt_required
-    def delete(self, resource):
-        data = request.get_json()
-        print(data)
-        identity = get_jwt_identity()
-        print(identity)
-        vendor = Vendor.find_by_uid(identity)
-        print(vendor)
-        catalog = Catalog.find_by_page_id(vendor.page_id)
-        print(catalog)
-        if not catalog:
-            return 'Catalog Not Found'
-            print('Catalog Not Found')
-        if resource == 'items':
-            catalog.remove_item(data['id'])
-            print('Item Removed Successfully')
-            return f'{resource} Removed successfully'
-        elif resource == 'categories':
-            catalog.remove_category(data['id'])
-            print('Category Removed Successfully')
-        return f'{resource} Removed successfully'
+#     @jwt_required
+#     def delete(self, resource):
+#         data = request.get_json()
+#         print(data)
+#         identity = get_jwt_identity()
+#         print(identity)
+#         vendor = Vendor.find_by_uid(identity)
+#         print(vendor)
+#         catalog = Catalog.find_by_page_id(vendor.page_id)
+#         print(catalog)
+#         if not catalog:
+#             return 'Catalog Not Found'
+#             print('Catalog Not Found')
+#         if resource == 'items':
+#             catalog.remove_item(data['id'])
+#             print('Item Removed Successfully')
+#             return f'{resource} Removed successfully'
+#         elif resource == 'categories':
+#             catalog.remove_category(data['id'])
+#             print('Category Removed Successfully')
+#         return f'{resource} Removed successfully'
 
 
-@api.route('/knowledge_base')
-class CatalogResourceKnowledgeBase(Resource):
-    @jwt_required
-    def get(self):
-        pass
+# @api.route('/knowledge_base')
+# class CatalogResourceKnowledgeBase(Resource):
+#     @jwt_required
+#     def get(self):
+#         pass
 
-    @jwt_required
-    def post(self):
-        pass
+#     @jwt_required
+#     def post(self):
+#         pass
 
-    @jwt_required
-    def put(self):
-        data = request.get_json()
-        print(data)
-        identity = get_jwt_identity()
-        print(identity)
-        vendor = Vendor.find_by_uid(identity)
-        print(vendor)
-        catalog = Catalog.find_by_page_id(vendor.page_id)
-        print(catalog)
-        catalog.edit_knowledge_value(
-            data['category'], data['key'], data['value'], vendor.page_access_token)
-        print('Knowlegebase Edited')
-        return 'knowledge value edited', 200
+#     @jwt_required
+#     def put(self):
+#         data = request.get_json()
+#         print(data)
+#         identity = get_jwt_identity()
+#         print(identity)
+#         vendor = Vendor.find_by_uid(identity)
+#         print(vendor)
+#         catalog = Catalog.find_by_page_id(vendor.page_id)
+#         print(catalog)
+#         catalog.edit_knowledge_value(
+#             data['category'], data['key'], data['value'], vendor.page_access_token)
+#         print('Knowlegebase Edited')
+#         return 'knowledge value edited', 200
 
-    @jwt_required
-    def delete(self):
-        pass
+#     @jwt_required
+#     def delete(self):
+#         pass
--- app/catalog/model.py
+++ app/catalog/model.py
@@ -1,10 +1,9 @@
-from sqlalchemy_json import NestedMutableJson
 import datetime
-import json
-from ..models.generic import GenericTemplate
-from ..models.bot import Bot
-from app import db
 from uuid import uuid1
+from app import db
+from sqlalchemy_json import NestedMutableJson
+from ..models.bot import Bot
+from ..models.generic import GenericTemplate
 
 
 class Catalog(db.Model):
@@ -14,58 +13,11 @@ class Catalog(db.Model):
     page_id = db.Column(db.String, db.ForeignKey(
         'vendors.page_id', ondelete='SET NULL', onupdate="CASCADE"), unique=True, nullable=True)
     blocks = db.Column(NestedMutableJson)
-    categories = db.Column(NestedMutableJson)
-    knowledge = db.Column(NestedMutableJson)
-    items = db.Column(NestedMutableJson)
+    categories = db.relationship(
+        'Category', backref='catalog', lazy='select')
 
     def __init__(self, page_id):
         self.page_id = page_id
-        self.items = {}
-        self.categories = {}
-        self.knowledge = {
-            'greetings': {
-                'label': 'Greetings',
-                'values':
-                    {
-                        'Welcome_Message':
-                        'Welcome How Can I help you?',
-                        'Thank_You': 'Thank You'
-                    }
-            },
-            'browse': {
-                'label': 'Browse',
-                'values': {'Order_Confirmation_Message': 'Order is Confirmed',
-                           'Business_Closed_Message': 'Business is Closed',
-                           'Info_Message': 'Address and Phone Number'}
-            },
-            # 'comments': {
-            #     'label': 'Comments',
-            #     'values':
-            #         {
-            #             'Ask_For_Menu': 'You Asked For Menu',
-            #             'Ask_For_Address': 'You Asked For Address'},
-            # },
-            'persistant_menu': {
-                'label': 'Persistant Menu',
-                'values':
-                    {'Show_Menu': 'Show Menu',
-                     'Show_Current_Order': 'Show Current Order'},
-
-            },
-            'buttons': {
-                'label': 'Buttons',
-                'values':
-                    {'Show_Info': 'Show Info',
-                     'Show_Sub_Menu': 'Sub Menu',
-                     'Show_Main_Menu': 'Main Menu',
-                     'Confirm_Order': 'Confirm Order',
-                     'Edit_Order': 'Edit Order',
-                     'Add_to_Order': 'Add to Order',
-                     'Cancel_Order': 'Cancel Order',
-                     'Track_Order': 'Track Order'
-                     }
-            }
-        }
         self.blocks = {
             'main_menu': {
                 'payload': {
@@ -75,7 +27,44 @@ class Catalog(db.Model):
             },
         }
         self.created_time = datetime.datetime.utcnow()
+        # self.knowledge = {
+        #     'greetings': {
+        #         'label': 'Greetings',
+        #         'values':
+        #             {
+        #                 'Welcome_Message':
+        #                 'Welcome How Can I help you?',
+        #                 'Thank_You': 'Thank You'
+        #             }
+        #     },
+        #     'browse': {
+        #         'label': 'Browse',
+        #         'values': {'Order_Confirmation_Message': 'Order is Confirmed',
+        #                    'Business_Closed_Message': 'Business is Closed',
+        #                    'Info_Message': 'Address and Phone Number'}
+        #     },
 
+        #     'persistant_menu': {
+        #         'label': 'Persistant Menu',
+        #         'values':
+        #             {'Show_Menu': 'Show Menu',
+        #              'Show_Current_Order': 'Show Current Order'},
+
+        #     },
+        #     'buttons': {
+        #         'label': 'Buttons',
+        #         'values':
+        #             {'Show_Info': 'Show Info',
+        #              'Show_Sub_Menu': 'Sub Menu',
+        #              'Show_Main_Menu': 'Main Menu',
+        #              'Confirm_Order': 'Confirm Order',
+        #              'Edit_Order': 'Edit Order',
+        #              'Add_to_Order': 'Add to Order',
+        #              'Cancel_Order': 'Cancel Order',
+        #              'Track_Order': 'Track Order'
+        #              }
+        #     }
+        #
     # Class Methods
 
     @classmethod
@@ -90,7 +79,7 @@ class Catalog(db.Model):
                 'elements': [
                     {
                         'title': self.knowledge['greetings']['values']['Welcome_Message'],
-                        'image_url': 'https://scontent-hbe1-1.xx.fbcdn.net/v/t1.0-9/p960x960/80390516_2962955900382554_7936731969642037248_o.jpg?_nc_cat=108&_nc_ohc=68iQXp-Lxn0AX9pBDCk&_nc_ht=scontent-hbe1-1.xx&_nc_tp=6&oh=f886d8238a13919294e79f9bfb70fa0b&oe=5EFDAE46',
+                        'image_url': '',
                         'subtitle': '',
                         'buttons': [{
                             'type': 'postback',
@@ -142,77 +131,58 @@ class Catalog(db.Model):
 
     # Categories Methods
 
-    def add_category(self, title, subtitle, img):
-        if len(self.blocks['main_menu']['payload']['elements']) == 13:
-            print('Categories exceeded max capacity')
-            return 'Categories Full'
-        _id = uuid1().hex
-        temp = {
-            'id': _id,
-            'title': title,
-            'subtitle': subtitle,
-            'img': img,
-            'block': self.make_category_block(_id, title, subtitle, img)
-        }
-        self.blocks[temp['id']] = {
-            'payload': {
-                'template_type': 'generic',
-                'elements': []
-            }
-        }
-        self.categories[_id] = temp
-        self.build_main_menu()
-        self.save()
+    # def add_category(self, title, subtitle, img):
+    #     if len(self.blocks['main_menu']['payload']['elements']) == 13:
+    #         print('Categories exceeded max capacity')
+    #         return 'Categories Full'
+    #     _id = uuid1().hex
+    #     self.blocks[temp['id']] = {
+    #         'payload': {
+    #             'template_type': 'generic',
+    #             'elements': []
+    #         }
+    #     }
+    #     self.categories[_id] = temp
+    #     self.build_main_menu()
+    #     self.save()
 
-    def remove_category(self, _id):
-        self.categories.pop(_id, None)
-        self.build_main_menu()
-        self.save()
+    # def remove_category(self, _id):
+    #     self.categories.pop(_id, None)
+    #     self.build_main_menu()
+    #     self.save()
 
-    def edit_category(self, category):
-        if category['id'].isnumeric():
-            category['id'] = uuid1().hex
-        category['block'] = self.make_category_block(
-            category['id'], category['title'], category['subtitle'], category['img'])
-        self.categories[category['id']] = category
-        self.build_main_menu()
-        self.save()
+    # def edit_category(self, category):
+    #     if category['id'].isnumeric():
+    #         category['id'] = uuid1().hex
+    #     category['block'] = self.make_category_block(
+    #         category['id'], category['title'], category['subtitle'], category['img'])
+    #     self.categories[category['id']] = category
+    #     self.build_main_menu()
+    #     self.save()
 
     # Items Methods
 
-    def add_item(self, category_id, title, subtitle, price, img, in_stock):
-        _id = uuid1().hex
-        category = self.categories[category_id]
-        temp = {
-            'id': _id,
-            'category_id': category_id,
-            'category_title': category['title'],
-            'title': title,
-            'subtitle': subtitle,
-            'price': price,
-            'in_stock': in_stock,
-            'img': img,
-            'block': self.make_item_block(_id, title, subtitle, price, img)
-        }
-        self.items[_id] = temp
-        self.build_category(category_id)
-        self.save()
+    # def add_item(self, category_id, title, subtitle, price, img, in_stock):
 
-    def remove_item(self, _id):
-        item = self.items[_id]
-        self.items.pop(_id, None)
-        if item['category_id'] in self.categories:
-            self.build_category(item['category_id'])
-        self.save()
+    #     self.items[_id] = temp
+    #     self.build_category(category_id)
+    #     self.save()
 
-    def edit_item(self, item):
-        if item['id'].isnumeric():
-            item['id'] = uuid1().hex
-        item['block'] = self.make_item_block(
-            item['id'], item['title'], item['subtitle'], item['price'], item['img'])
-        self.build_category(item['category_id'])
-        self.items[item['id']] = item
-        self.save()
+    # def remove_item(self, _id):
+    #     item = self.items[_id]
+    #     self.items.pop(_id, None)
+    #     if item['category_id'] in self.categories:
+    #         self.build_category(item['category_id'])
+    #     self.save()
+
+    # def edit_item(self, item):
+    #     if item['id'].isnumeric():
+    #         item['id'] = uuid1().hex
+    #     item['block'] = self.make_item_block(
+    #         item['id'], item['title'], item['subtitle'], item['price'], item['img'])
+    #     self.build_category(item['category_id'])
+    #     self.items[item['id']] = item
+    #     self.save()
 
     # Knowledge Methods
 
@@ -243,66 +213,37 @@ class Catalog(db.Model):
         db.session.commit()
 
     # Menu Building Methods
-    def build_main_menu(self):
-        temp = []
-        print(self.categories.items())
-        for k, v in self.categories.items():
-            if v['id'] in self.blocks and self.blocks[v['id']]['payload']['elements'] is not None:
-                print(k)
-                print(v)
-                if 'block' not in v:
-                    v['block'] = self.make_category_block(
-                        v['id'], v['title'], v['subtitle'], v['img'])
-                temp.append(
-                    v['block'])
+    # def build_main_menu(self):
+    #     temp = []
+    #     print(self.categories.items())
+    #     for k, v in self.categories.items():
+    #         if v['id'] in self.blocks and self.blocks[v['id']]['payload']['elements'] is not None:
+    #             print(k)
+    #             print(v)
+    #             if 'block' not in v:
+    #                 v['block'] = self.mitemsake_category_block(
+    #                     v['id'], v['title'], v['subtitle'], v['img'])
+    #             temp.append(
+    #                 v['block'])
 
-        self.blocks['main_menu']['payload']['elements'] = temp
+    #     self.blocks['main_menu']['payload']['elements'] = temp
 
-    def build_category(self, _id):
-        temp = []
-        category = self.categories[_id]
-        for k, v in self.items.items():
-            print(v)
-            if v['category_id'] == _id and v['in_stock']:
-                v['block'] = self.make_item_block(
-                    v['id'], v['title'], v['subtitle'], v['price'], v['img'])
-                temp.append(
-                    v['block'])
-        self.blocks[_id]['payload']['elements'] = temp
+    # def build_category(self, _id):
+    #     temp = []
+    #     category = self.categories[_id]
+    #     for k, v in self.items.items():
+    #         print(v)
+    #         if v['category_id'] == _id and v['in_stock']:
+    #             v['block'] = self.make_item_block(
+    #                 v['id'], v['title'], v['subtitle'], v['price'], v['img'])
+    #             temp.append(
+    #                 v['block'])
+    #     self.blocks[_id]['payload']['elements'] = temp
 
     # Block building functions
 
-    def make_item_block(self, _id, title, subtitle, price, img):
-        return {
-            'title': title,
-            'image_url': img,
-            'subtitle': subtitle,
-            'buttons': [{
-                'type': 'web_url',
-                'title': f'اطلب بـ{price}ج',
-                'url': f'https://rest-bot-dev.herokuapp.com/webview/order/{_id}',
-                'webview_height_ratio': 'tall',
-                'messenger_extensions': 'true'
-            }]
-        }
-
-    def make_category_block(self, _id, title, subtitle, img):
-        print(self.knowledge)
-        return {
-            'title': title,
-            'subtitle': subtitle,
-            'image_url': img,
-            'buttons': [
-                {
-                    "type": "postback",
-                    "title": self.knowledge['buttons']['values']['Show_Sub_Menu'],
-                    "payload": _id
-                }
-            ]
-        }
-
-    def build_blocks(self):
-        self.set_get_started()
-        self.build_main_menu()
-        for title, category in self.categories.items():
-            self.build_category(category['id'])
+    # def build_blocks(self):
+    #     self.set_get_started()
+    #     self.build_main_menu()
+    #     for title, category in self.categories.items():
+    #         self.build_category(category['id'])
