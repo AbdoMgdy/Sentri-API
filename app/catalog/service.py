b234ff75b3029458929ee0c53744c87e6d472c4d
AbdoMgdy <abdoman46@hotmail.com>
2020-09-30 21:21:51 +0200

    add catalog service

---
+++ app/catalog/service.py
@@ -0,0 +1,15 @@
+from ..category import Category
+
+
+class CatalogService():
+    @staticmethod
+    def build_main_menu():
+        pass
+
+    @staticmethod
+    def buid_block(catalog):
+        categories = Category.find_by_uuid(catalog.uuid)
+        block_elements = catalog.block['payload']['elements']
+        for category in categories:
+            block_elements.append(category.block)
+        catalog.save()
