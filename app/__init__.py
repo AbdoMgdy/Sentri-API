beaefa51056ac315331cffbd7b78d088cc297429
AbdoMgdy <abdoman46@hotmail.com>
2020-10-04 15:56:46 +0200

    remove unneede package

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
 from flask_migrate import Migrate
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS 
 from flask_jwt_extended import JWTManager
 
 from flask_marshmallow import Marshmallow
